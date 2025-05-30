8449c13646decbc4365111aca11d83107b98f5de
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-05-30 13:17:14 +0400

    implement basic HTTP server with "Hello World" response

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+import http from "http";
 
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/plain",
+  });
+  res.end("Hello World");
+});
+
+server.listen(4040, "localhost", () => {
+  console.log("server is running on http://localhost:4040");
+});
