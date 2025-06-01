10ce408263ed115d6c388a7c47750770bc665783
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-06-01 13:26:43 +0400

    Refactor index.js to update route handling and remove unused endpoints

--- index.js
+++ index.js
@@ -6,16 +6,8 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/products", (req, res) => {
   res.send("Hello World");
-});
-
-app.get("/about", (req, res) => {
-  res.send("About Page");
-});
-
-app.get("/posts", (req, res) => {
-  res.json(JSON.parse(posts));
 });
 
 app.listen(4040, "localhost", () => {
