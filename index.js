62d74da4ebcbb7ac020b2b071c6031f160ba7d80
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-06-03 13:46:21 +0400

    Fix MongoDB connection URL to use the correct port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import mongoose from "mongoose";
 const app = express();
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/myapp", {
+mongoose.connect("mongodb://http://localhost:4040/myapp", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
