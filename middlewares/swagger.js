98997d2b17bfe4740336ef172e50e2fed3d88aaf
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-06-07 18:46:41 +0400

    feat: add swagger middleware for API documentation

---
+++ middlewares/swagger.js
@@ -0,0 +1,19 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+1;
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Documentation",
+      description: "API documentation for the application",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"], // Path to the API docs
+};
