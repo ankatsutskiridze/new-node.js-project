e91a067b605e1676d2a06f2c55d06d3e5bab547b
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-06-08 12:45:04 +0400

    refactor: restructure swagger options for clarity

--- middlewares/swagger.js
+++ middlewares/swagger.js
@@ -1,16 +1,19 @@
 import swaggerJSDoc from "swagger-jsdoc";
 
 const options = {
-  openapi: "3.0.0",
-  info: {
-    title: "Your API Title",
-    description: "API documentation",
-  },
-  servers: [
-    {
-      url: "http://localhost:3000",
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Your API Title",
+      description: "API documentation",
     },
-  ],
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"], // შეცვალე შენს ფაილებზე
 };
 
 const specs = swaggerJSDoc(options);
